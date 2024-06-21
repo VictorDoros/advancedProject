464cf67d64ebdb1426b54335710ac75160ec897e
Victor Doros <dorosvictor@gmail.com>
2024-06-21 16:33:12 +0300

    install cy-step

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -16,6 +16,9 @@
 // Import commands.js using ES2015 syntax:
 import "./commands"
 import "cypress-mochawesome-reporter/register"
+import 'cypress-plugin-steps'
+const registerCypressGrep = require('@cypress/grep')
+registerCypressGrep()
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
